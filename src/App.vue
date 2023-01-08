036f5a7b0639a34793ebbede873db877da86675e
Carlos Ferrus <charlitosf@gmail.com>
2023-01-08 15:29:26 +0100

    Improved overall page style

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,40 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-        <RouterLink to="/login">Login</RouterLink>
-      </nav>
-    </div>
-  </header>
-
+  <div class="navbar">
+    <RouterLink to="/">Home</RouterLink>
+    <RouterLink to="/login">Login</RouterLink>
+    <RouterLink class="about" to="/about">About</RouterLink>
+  </div>
   <RouterView />
 </template>
 
 <style scoped>
+.navbar {
+  display: flex;
+  width: 100%;
+  justify-content: space-between;
+  background-color: rgb(64, 64, 64);
+}
+
+.navbar a {
+  padding: 1rem;
+}
+
+div a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+div a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+.about {
+  margin-left: auto;
+}
+
 header {
   line-height: 1.5;
   max-height: 100vh;
