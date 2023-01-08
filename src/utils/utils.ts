4a71b0e39369b2751723579da98a8dca661424c8
Carlos Ferrus <charlitosf@gmail.com>
2023-01-08 17:28:13 +0100

    Game creation

---
+++ src/utils/utils.ts
@@ -0,0 +1,5 @@
+function generate4DigitRandomNumber() {
+    return Math.floor(1000 + Math.random() * 9000).toString();
+}
+
+export {generate4DigitRandomNumber};
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,52 @@
 <script setup lang="ts">
 import { useRouter } from 'vue-router';
+import { getDatabase, ref as fRef, runTransaction } from 'firebase/database';
+import { useUserStore } from '@/stores/user';
+import { generate4DigitRandomNumber } from '@/utils/utils';
 
 const router = useRouter();
 
-const onStartGame = () => {
+const onStartGame = async () => {
+  const db = getDatabase();
+  const rootRef = fRef(db);
+  const userStore = useUserStore();
+  await runTransaction(rootRef, (currentData) => {
+    if (currentData === null) {
+      const gameCode = generate4DigitRandomNumber();
+      return {
+        games: {
+          [gameCode]: {
+            [userStore.user?.uid!]: true
+          }
+        },
+        [userStore.user?.uid!]: {
+          [gameCode]: {
+            gameStarted: false,
+            // White players: []
+            // Word: ''
+          },
+        },
+      };
+    } else {
+      let existingGames: string[] = [];
+      if (currentData.games) {
+        existingGames = Object.keys(currentData.games);
+      }
+      let gameCode = generate4DigitRandomNumber();
+      while (existingGames.includes(gameCode)) {
+        gameCode = generate4DigitRandomNumber();
+      }
+      currentData.games[gameCode] = {
+        [userStore.user?.uid!]: true
+      };
+      currentData[userStore.user?.uid!][gameCode] = {
+        gameStarted: false,
+        // White players: []
+        // Word: ''
+      };
+      return currentData;
+    }
+  })
   router.push('/lobby');
 };
 
