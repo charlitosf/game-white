e5e80eda3e3c2a44f82e5680c0bd15a7225adecd
Carlos Ferrus <charlitosf@gmail.com>
2023-01-08 16:46:17 +0100

    Created actual project structure

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <h1>Welcome to the game "White"!</h1>
+  <button>Start new game</button>
+  <button>Join game</button>
+  My currently started games:
 </template>
