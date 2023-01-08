f5d292f4ee87a6fb730a1a9744d670ea6b1b0081
Carlos Ferrus <charlitosf@gmail.com>
2023-01-08 14:33:33 +0100

    Login now pretty

---
+++ src/views/LoginView.vue
@@ -0,0 +1,161 @@
+<template>
+<div class="login-page">
+   <div class="wallpaper"></div>
+
+   <div class="container">
+      <div v-if="!registerActive" class="card login" v-bind:class="{ error: emptyFields }">
+         <h1>Sign In</h1>
+         <form class="form-group">
+            <input v-model="emailLogin" type="email" class="form-control" placeholder="Email" required>
+            <input v-model="passwordLogin" type="password" class="form-control" placeholder="Password" required>
+            <input type="submit" class="btn" @click="doLogin">
+            <p>Don't have an account? <a href="#" @click="registerActive = !registerActive, emptyFields = false">Sign up here</a>
+            </p>
+         </form>
+      </div>
+
+      <div v-else class="card register" v-bind:class="{ error: emptyFields }">
+         <h1>Sign Up</h1>
+         <form class="form-group">
+            <input v-model="emailReg" type="email" class="form-control" placeholder="Email" required>
+            <input v-model="passwordReg" type="password" class="form-control" placeholder="Password" required>
+            <input v-model="confirmReg" type="password" class="form-control" placeholder="Confirm Password" required>
+            <input type="submit" class="btn" @click="doRegister">
+            <p>Already have an account? <a href="#" @click="registerActive = !registerActive, emptyFields = false">Sign in here</a>
+            </p>
+         </form>
+      </div>
+   </div>
+</div>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue';
+
+const emailReg = ref('');
+const passwordReg = ref('');
+const confirmReg = ref('');
+
+const emailLogin = ref('');
+const passwordLogin = ref('');
+
+const registerActive = ref(false);
+const emptyFields = ref(false);
+
+const doRegister = () => {
+   if (emailReg.value == '' || passwordReg.value == '' || confirmReg.value == '') {
+      emptyFields.value = true;
+   } else {
+      emptyFields.value = false;
+      // Register using firebase auth
+      
+   }
+}
+
+const doLogin = () => {
+   if (emailLogin.value == '' || passwordLogin.value == '') {
+      emptyFields.value = true;
+   } else {
+      emptyFields.value = false;
+   }
+}
+</script>
+
+<style scoped>
+.container {
+   margin: 0 auto;
+   max-width: 100%;
+   padding: 0 1rem;
+}
+
+p {
+   line-height: 2rem;
+}
+
+.btn {
+   background-color: rgb(10, 96, 50);
+   border: 1px solid rgb(6, 52, 28);
+   border-radius: 0;
+   color: #fff;
+   font-size: 1rem;
+   font-weight: 700;
+   margin-top: 10px;
+   padding: 10px 20px;
+   transition-duration: 0.4s;
+}
+
+.btn:hover {
+   background-color: rgb(6, 52, 28);
+   border: 1px solid rgb(10, 96, 50);
+   color: #fff;
+}
+
+.card {
+   padding: 30px;
+   background-color: rgb(37, 37, 37);
+}
+
+.form-group {
+   display: flex;
+   flex-direction: column;
+}
+
+.form-control {
+  margin-bottom: 20px;
+  padding: 12px 10px;
+}
+
+.form-control:focus {
+  border-color: rgb(10, 96, 50);
+  box-shadow: 0 0 0 0.2rem rgb(10, 96, 50);
+}
+
+.login-page {
+  align-items: center;
+  display: flex;
+  height: 100vh;
+}
+
+.fade-enter-active, .fade-leave-active {
+  transition: opacity .5s;
+}
+
+h1 {
+  margin-bottom: 1.5rem;
+}
+
+
+.wallpaper {
+  background: rgb(10, 96, 50);
+  height: 100%;
+  position: absolute;
+  width: 100%;
+}
+
+.fade-enter, .fade-leave-to {
+  opacity: 0;
+}
+
+.error {
+   animation-name: errorShake;
+   animation-duration: 0.3s;
+}
+
+@keyframes errorShake {
+   0% {
+      transform: translateX(-25px);
+   }
+   25% {
+      transform: translateX(25px);
+   }
+   50% {
+      transform: translateX(-25px);
+   }
+   75% {
+      transform: translateX(25px);
+   }
+   100% {
+      transform: translateX(0);
+   }
+}
+</style>
