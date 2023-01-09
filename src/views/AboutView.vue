48ae2072c06e1adf6c2c45fdb42ccfa50dd81f44
Carlos Ferrus <charlitosf@gmail.com>
2023-01-09 23:57:54 +0100

    Added info to the about page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,17 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>The game</h1>
+    <p>
+      This is a really simple game called "White" that requires a group of minimum 4 players that can communicate with one another. One of them will be the "narrator" (or the "admin", as it is referred to in this application) and the rest will be the "players". In each round, the narrator will choose a minimum of one of the players and designate them as "white". Then, the narrator will choose a word or a single concept which will be shared to the rest of the players except for the "white" in secret. All the players will then, by turns, say a word or concept related to the initial one. After all players have said a word, they will have to vote which of them they think it is a "white". The player who gets the most votes will be eliminated and the game will continue until there are no more "whites" left or there are the same amount of "whites" as players. The goal of the players is to find out who the "whites" are and eliminate them, while the goal of the "whites" is to survive until the end. As an additional rule, if a "white" says exactly the same word as the initial one, they will instantly win the game. Moreover, if a player says the initial word exactly, all players will lose.
+    </p>
+    <h1>The developer</h1>
+    <p>
+      My name is Carlos Ferrús, a computer scientist from Alicante, Spain. I hold a Degree in Computer Science specialized in Software Engineering and a Masters Degree in Cybersecurity. I spend my free time developing a variety of projects, from tiny automation scripts to full-fledged web applications. I also enjoy traveling, as shown in the picture taken in Lanzarote. You can find the source code of this application in my GitHub as well as other projects I have worked on. I can also be contacted through LinkedIn and email with the appropriate buttons. I hope you enjoy this game as much as I enjoyed developing it!
+    </p>
+    <img src="@/assets/pic.jpg" alt="Me">
+    <a href="https://www.linkedin.com/in/carlos-ferrus/">LinkedIn</a>
+    <a href="mailto:carlos@ferrus.es">Email</a>
+    <a href="https://github.com/charlitosf">GitHub</a>
   </div>
 </template>
 
