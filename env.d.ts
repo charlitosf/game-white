908e0fbe0fe1fc2b34e30a6ad0b3e5bbf3fa5efa
Carlos Ferrus <charlitosf@gmail.com>
2023-01-10 18:10:49 +0100

    Added appcheck

--- env.d.ts
+++ env.d.ts
@@ -2,6 +2,8 @@
 
 interface ImportMetaEnv {
     readonly VITE_FIREBASE_API_KEY: string
+    readonly VITE_FIREBASE_CAPTCHA_KEY: string
+    readonly VITE_ENVIRONMENT: string
 }
 
 interface ImportMeta {
--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ import router from './router'
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeAppCheck, ReCaptchaV3Provider } from 'firebase/app-check';
 
 import './assets/main.css'
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
@@ -15,6 +16,7 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons'
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { connectDatabaseEmulator, getDatabase } from 'firebase/database'
 
 
 // Your web app's Firebase configuration
@@ -37,6 +39,15 @@ app.use(createPinia())
 app.use(router)
 // Initialize Firebase
 const fApp = initializeApp(firebaseConfig);
+// Pass your reCAPTCHA v3 site key (public key) to activate(). Make sure this
+// key is the counterpart to the secret key you set in the Firebase console.
+initializeAppCheck(fApp, {
+  provider: new ReCaptchaV3Provider(import.meta.env.VITE_FIREBASE_CAPTCHA_KEY),
+
+  // Optional argument. If true, the SDK automatically refreshes App Check
+  // tokens as needed.
+  isTokenAutoRefreshEnabled: true
+});
 const auth = getAuth(fApp);
 onAuthStateChanged(auth, (user) => {
   const userStore = useUserStore();
@@ -47,6 +58,10 @@ onAuthStateChanged(auth, (user) => {
     router.push('/login')
   }
 });
+if (import.meta.env.VITE_ENVIRONMENT === 'DEV') { 
+  const db = getDatabase(fApp);
+  connectDatabaseEmulator(db, 'localhost', 9000);
+}
 
 library.add(faEnvelope, faLinkedin, faGithub);
 
