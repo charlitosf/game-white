641d7adff5f79b7fc2f17bf4b98daf5979b19e70
Carlos Ferrus <charlitosf@gmail.com>
2023-01-13 17:24:01 +0100

    Quick hotfix

--- src/main.ts
+++ src/main.ts
@@ -27,16 +27,17 @@ app.use(router)
 
 // Initialize Firebase
 const fApp = initializeApp(config);
-if (import.meta.env.DEV) {
-  // Pass your reCAPTCHA v3 site key (public key) to activate(). Make sure this
-  // key is the counterpart to the secret key you set in the Firebase console.
+if (import.meta.env.PROD) {
   initializeAppCheck(fApp, {
+    // Pass your reCAPTCHA v3 site key (public key) to activate(). Make sure this
+    // key is the counterpart to the secret key you set in the Firebase console.
     provider: new ReCaptchaV3Provider(import.meta.env.VITE_FIREBASE_CAPTCHA_KEY),
 
     // Optional argument. If true, the SDK automatically refreshes App Check
     // tokens as needed.
     isTokenAutoRefreshEnabled: true
   });
+} else if (import.meta.env.DEV) {
   const db = getDatabase(fApp);
   connectDatabaseEmulator(db, 'localhost', 9000);
 
