d5c44c12e5165715c09818c05841892a9e6b3348
Carlos Ferrus <charlitosf@gmail.com>
2023-01-13 15:51:27 +0100

    Extracted auth stuff from main to user store

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import { useUserStore } from '@/stores/user'
 
 import App from './App.vue'
 import router from './router'
@@ -10,7 +9,6 @@ import { initializeApp } from "firebase/app";
 import { initializeAppCheck, ReCaptchaV3Provider } from 'firebase/app-check';
 
 import './assets/main.css'
-import { getAuth, onAuthStateChanged } from 'firebase/auth';
 
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons'
@@ -37,11 +35,12 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+
 // Initialize Firebase
 const fApp = initializeApp(firebaseConfig);
-// Pass your reCAPTCHA v3 site key (public key) to activate(). Make sure this
-// key is the counterpart to the secret key you set in the Firebase console.
 if (import.meta.env.DEV) {
+  // Pass your reCAPTCHA v3 site key (public key) to activate(). Make sure this
+  // key is the counterpart to the secret key you set in the Firebase console.
   initializeAppCheck(fApp, {
     provider: new ReCaptchaV3Provider(import.meta.env.VITE_FIREBASE_CAPTCHA_KEY),
 
@@ -49,18 +48,6 @@ if (import.meta.env.DEV) {
     // tokens as needed.
     isTokenAutoRefreshEnabled: true
   });
-}
-const auth = getAuth(fApp);
-onAuthStateChanged(auth, (user) => {
-  const userStore = useUserStore();
-  userStore.user = user;
-  if (user) {
-    router.push('/')
-  } else {
-    router.push('/login')
-  }
-});
-if (import.meta.env.DEV) { 
   const db = getDatabase(fApp);
   connectDatabaseEmulator(db, 'localhost', 9000);
 }
--- src/stores/user.ts
+++ src/stores/user.ts
@@ -1,10 +1,22 @@
-import type { User } from "firebase/auth"
+import { getAuth, onAuthStateChanged, type User } from "firebase/auth"
 import { defineStore } from "pinia"
 import { ref, type Ref } from "vue"
+import { useRouter } from "vue-router";
 
 export const useUserStore = defineStore('user', () => {
-    const user: Ref<User | null> = ref(null)
-  
-    return { user }
-  })
+  const router = useRouter();
+  const auth = getAuth();
+
+  const user: Ref<User | null> = ref(null);
+
+  onAuthStateChanged(auth, (newUser) => {
+    user.value = newUser;
+    if (newUser) {
+      router.push('/')
+    } else {
+      router.push('/login')
+    }
+  });
+  return { user }
+})
   
