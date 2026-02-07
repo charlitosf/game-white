e41a0719577cee496a579e19c348dd894842a43b
Carlos Ferrus <charlitosf@gmail.com>
2026-02-07 22:32:02 +0100

    Added copy game link button

--- src/views/GameView.vue
+++ src/views/GameView.vue
@@ -14,17 +14,40 @@ if (gameStore.gameId === null) {
 const onLeaveGame = () => {
   gameStore.leaveGame();
 };
+const onDeleteGame = () => {
+  gameStore.deleteGame();
+};
+const onCopyGameLink = () => {
+  navigator.clipboard.writeText(window.location.href);
+  alert("Game link copied to clipboard");
+};
 </script>
 
 <template>
-  <button
-    v-if="!gameStore.amIAdmin"
-    @click="onLeaveGame"
-    class="btn btn-danger mb-1"
-  >
-    Leave game
-  </button>
+  <div class="top-bar">
+    <button
+      v-if="!gameStore.amIAdmin"
+      @click="onLeaveGame"
+      class="btn btn-danger mb-1"
+    >
+      Leave game
+    </button>
+    <button v-else @click="onDeleteGame" class="btn btn-danger mb-1">
+      Delete game
+    </button>
+
+    <button @click="onCopyGameLink" class="btn btn-primary mb-1">
+      Copy game link
+    </button>
+  </div>
 
   <LobbyComponent v-if="!gameStore.gameStarted" />
   <GameComponent v-else />
 </template>
+
+<style lang="css" scoped>
+.top-bar {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
