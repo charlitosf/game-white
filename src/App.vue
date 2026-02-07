52e839dbb699be06255496af19211aebfdd44f80
Carlos Ferrus <charlitosf@gmail.com>
2026-02-07 20:10:29 +0100

    Extracted navbar

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,14 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from "vue-router";
-import { useUserStore } from "@/stores/user";
-
-const userStore = useUserStore();
-
-const onLogout = () => {
-  userStore.logout();
-};
+import { RouterView } from "vue-router";
+import NavbarComponent from "./components/NavbarComponent.vue";
 </script>
 
 <template>
-  <div class="navbar">
-    <RouterLink v-if="userStore.user" :to="{ name: 'home' }">Home</RouterLink>
-    <RouterLink v-if="!userStore.user" :to="{ name: 'login-anonymous' }"
-      >Login</RouterLink
-    >
-    <RouterLink to="/about">About</RouterLink>
-    <a v-if="userStore.user" href="#" @click="onLogout">Logout</a>
-  </div>
+  <NavbarComponent />
   <RouterView />
 </template>
 
 <style>
-.navbar {
-  display: flex;
-  width: 100%;
-  justify-content: space-between;
-  background-color: rgb(64, 64, 64);
-}
-
-.navbar a {
-  padding: 1rem;
-}
-
-div a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-div a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
 .btn {
   border-radius: 0;
   color: #fff;
